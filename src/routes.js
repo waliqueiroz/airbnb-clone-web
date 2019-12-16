63492dcc523f758cb0679306b5efaf945bf9c881
Wali Queiroz <wsantos077@gmail.com>
2019-12-16 12:24:48 -0300

    Tela inicial e mapa

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./services/auth";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import App from "./pages/App";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -23,7 +24,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route path="/signup" component={SignUp} />
-      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+      <PrivateRoute path="/app" component={App} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
